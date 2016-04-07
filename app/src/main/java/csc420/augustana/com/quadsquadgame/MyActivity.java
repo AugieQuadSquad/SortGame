569604a405fbaa4e12ec2459d8d86bddfeb6144d
Devon <devon.white3@gmail.com>
2016-04-07 15:03:57 -0500

    test commit

--- app/src/main/java/csc420/augustana/com/quadsquadgame/MyActivity.java
+++ app/src/main/java/csc420/augustana/com/quadsquadgame/MyActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 
 public class MyActivity extends AppCompatActivity {
 
+    //test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
