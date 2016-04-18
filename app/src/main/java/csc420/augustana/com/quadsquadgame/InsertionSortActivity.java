fca7c22808966ca6db81bc3057d891876545d919
Devon <devon.white3@gmail.com>
2016-04-17 20:11:52 -0500

    Added Tutorial for the Human sort.

--- app/src/main/java/csc420/augustana/com/quadsquadgame/InsertionSortActivity.java
+++ app/src/main/java/csc420/augustana/com/quadsquadgame/InsertionSortActivity.java
@@ -2,7 +2,6 @@ package csc420.augustana.com.quadsquadgame;
 
 // Source code based off http://androidsrc.net/android-view-drag-drop-functionality-sample-application/
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -20,7 +20,8 @@ import com.plattysoft.leonids.ParticleSystem;
 
 import java.util.Arrays;
 
-
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
 
 
 public class InsertionSortActivity extends AppCompatActivity implements View.OnTouchListener, View.OnDragListener {
@@ -33,11 +34,37 @@ public class InsertionSortActivity extends AppCompatActivity implements View.OnT
     private int[] countUpper = new int[15];
     private int[] countLower = new int[15];
 
+    private Button next;
+    private Button reset;
+    private Button test;
+    private ImageView image1;
+    private ImageView image2;
+    private ImageView image3;
+    private ImageView image4;
+    private LinearLayout container1;
+    private LinearLayout container2;
+    private LinearLayout container3;
+    private LinearLayout container4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insertion_sort);
         ItemDatabase.setValues();
+
+        next = (Button) findViewById(R.id.nextbutton);
+        reset = (Button) findViewById(R.id.resetButton);
+        test = (Button) findViewById(R.id.test);
+
+        image1 = (ImageView) findViewById(R.id.box_view1);
+        image2 = (ImageView) findViewById(R.id.box_view2);
+        image3 = (ImageView) findViewById(R.id.box_view3);
+        image4 = (ImageView) findViewById(R.id.box_view4);
+
+        container1 = (LinearLayout) findViewById(R.id.container_one);
+        container2 = (LinearLayout) findViewById(R.id.container_two);
+        container3 = (LinearLayout) findViewById(R.id.container_three);
+        container4 = (LinearLayout) findViewById(R.id.container_four);
 
 
         for (int i = 0; i < ItemDatabase.value.length; i++) {
@@ -282,5 +309,26 @@ public class InsertionSortActivity extends AppCompatActivity implements View.OnT
 
     public void displayMessage(String string){
         Toast.makeText(this, (string), Toast.LENGTH_SHORT).show();
+    }
+
+    public void Tutorial(View view){
+        ShowcaseConfig config = new ShowcaseConfig();
+        config.setDelay(500); //Delay is in milliseconds
+
+        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(this);
+
+        sequence.setConfig(config);
+
+        sequence.addSequenceItem(next, "Click the next button to scroll through the items to see their values", "Got it");
+
+        sequence.addSequenceItem(image1, "Drag the items to the bottom", "Got It");
+
+        sequence.addSequenceItem(container1, "In smallest to largest order, based on the values", "Got It");
+
+        sequence.addSequenceItem(reset, "If you mess up, don't worry! Just reset the game and try again", "Got It");
+
+        sequence.addSequenceItem(test, "If you think you've got it, find out with the test button!", "Close");
+
+        sequence.start();
     }
 }
