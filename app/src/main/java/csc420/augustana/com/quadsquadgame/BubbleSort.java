88ad545e75fca1f5a6b29cd2b5cf1351ebd6a0eb
Devon <devon.white3@gmail.com>
2016-04-20 15:50:43 -0500

    added animations to swap method

--- app/src/main/java/csc420/augustana/com/quadsquadgame/BubbleSort.java
+++ app/src/main/java/csc420/augustana/com/quadsquadgame/BubbleSort.java
@@ -3,19 +3,27 @@ package csc420.augustana.com.quadsquadgame;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import java.util.Arrays;
 import android.view.View;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 public class BubbleSort extends AppCompatActivity {
 
     public static TextView[] items = new TextView[8];
+    TextView clicked1st = null;
+    Animation animAlpha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubble_sort);
         ItemDatabase.setValues();
+
 
         TextView item1 = (TextView) findViewById(R.id.item1);
         items[0] = item1;
@@ -42,8 +50,11 @@ public class BubbleSort extends AppCompatActivity {
         items[7] = item8;
         item8.setTag(R.drawable.box_eight);
 
+        animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_translate);
+
         for(int i = 0; i < items.length; i++){
             items[i].setText(ItemDatabase.value[i] + "");
+            items[i].setOnClickListener(myClickListener);
         }
     }
 
@@ -56,7 +67,29 @@ public class BubbleSort extends AppCompatActivity {
         item2TV.setBackgroundResource(tempBackground);
         item1TV.setTag(item2TV.getTag());
         item2TV.setTag(tempBackground);
+        YoYo.with(Techniques.SlideInDown)
+                .duration(700)
+                .playOn(item1TV);
+        YoYo.with(Techniques.SlideInDown)
+                .duration(700)
+                .playOn(item2TV);
     }
 
+    public static void testSwap(View view){
+        swap(items[0], items[1]);
+    }
+
+    private View.OnClickListener myClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(clicked1st == null){
+                clicked1st = (TextView) v;
+                v.startAnimation(animAlpha);
+            } else{
+                swap(clicked1st, (TextView) v);
+                clicked1st = null;
+            }
+        }
+    };
 
 }
