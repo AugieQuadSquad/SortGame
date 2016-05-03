3c0eaeaa28321ea3e07723507c1811ffe16e002c
Catherine <catherinecross12@augustana.edu>
2016-05-02 22:27:44 -0500

    Added call to fill TextView for Instructions activity. Needs to be tested

--- app/src/main/java/csc420/augustana/com/quadsquadgame/Instructions.java
+++ app/src/main/java/csc420/augustana/com/quadsquadgame/Instructions.java
@@ -3,8 +3,11 @@ package csc420.augustana.com.quadsquadgame;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Instructions extends AppCompatActivity {
+    private final int BUBBLESORT = 0;
+    private final int INSERTIONSORT = 1;
 
     int currentGame = -1;
 
@@ -16,11 +19,13 @@ public class Instructions extends AppCompatActivity {
         Intent intent = getIntent();
         currentGame = intent.getIntExtra("game", -1);
 
-        if(currentGame == 0 ){
+        TextView temp = (TextView) findViewById(R.id.instructionTextView);
 
+        if(currentGame == 0 ){
+            temp.setText(R.string.bubblesortinstructions);
         } else if(currentGame == 1){
 
-        } else if(currentGame == 2){
+        } else if(currentGame == BUBBLESORT){
 
         } else {
 
